1282cfa5029966f3df4a8a1a3a88912c251f166c
Heikki Haveri <heikki.haveri@pastillilabs.com>
2024-09-02 15:09:07 +0300

    Load android plugins based on architecture

--- lib/src/android/util.cpp
+++ lib/src/android/util.cpp
@@ -8,8 +8,7 @@ namespace Util {
 
 QString pluginPath(const QString& id) {
     const QDir pluginDir(QCoreApplication::applicationDirPath());
-
-    return pluginDir.absoluteFilePath(QStringLiteral("libsituations%1.so").arg(id));
+    return pluginDir.absoluteFilePath(QStringLiteral("libsituations%1_%2.so").arg(id, QStringLiteral(ANDROID_ABI)));
 }
 
 } // namespace Util
