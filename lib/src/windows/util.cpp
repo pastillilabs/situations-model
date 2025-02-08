ac06d7c4e1db1d9aee94af4ab7e68f6dd4c88523
Heikki Haveri <heikki.haveri@gmail.com>
2025-02-08 02:25:14 +0200

    Update Windows pulugin path

--- lib/src/windows/util.cpp
+++ lib/src/windows/util.cpp
@@ -8,14 +8,9 @@ namespace Util {
 
 QString pluginPath(const QString& id) {
     QDir pluginDir(QCoreApplication::applicationDirPath());
+    pluginDir.cd(QStringLiteral("../plugins/%1").arg(id));
 
-#ifdef QT_DEBUG
-    pluginDir.cd(QStringLiteral("../../plugins/%1/debug").arg(id));
-#else
-    pluginDir.cd(QStringLiteral("../../plugins/%1/release").arg(id));
-#endif // QT_DEBUG
-
-    return pluginDir.absoluteFilePath(QStringLiteral("situations2%1.dll").arg(id));
+    return pluginDir.absoluteFilePath(QStringLiteral("situations%1.dll").arg(id));
 }
 
 } // namespace Util
